80740ac710a9e853b3a7a9c9844b852a14af2402
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-05-06 14:52:11 +0200

    New-TextBox

--- Modules/Graphics/Graphics.TextBox.psm1
+++ Modules/Graphics/Graphics.TextBox.psm1
@@ -1,5 +1,5 @@
 #
-# Module 'Graphics.Groupbox.psm1'
+# Module 'Graphics.TextBox.psm1'
 # Last Update: May 2026
 #
 
@@ -68,23 +68,19 @@ function Add-TextBoxDimensions {
 ####################################################################################################
 <#
 .SYNOPSIS
-    This function creates a new GroupBox on a specified ParentTabPage with specified properties.
+    This function creates a new TextBox.
 .DESCRIPTION
-    This function creates a new GroupBox on a specified ParentTabPage with specified properties such as Title, NumberOfRows, Color, and optionally places it above another GroupBox or on a SubTab.
-    The dimensions of the GroupBox are calculated based on the NumberOfRows and the settings defined in the ApplicationObject.
+    This function is part of the Application Delivery Assistant. It creates a new TextBox and adds it to the specified parent GroupBox.
 .EXAMPLE
-    New-GroupBox -ParentTabPage $MyTabPage -Title 'Application Input'
-.EXAMPLE
-    New-GroupBox -ParentTabPage $MyTabPage -Title 'MECM Input' -NumberOfRows 8 -Color 'Green' -GroupBoxAbove $MyOtherGroupbox
+    Invoke-TextBox -ParentGroupBox $MyGroupBox -RowNumber 2 -SizeType 'Medium' -Type 'Input' -Label 'Enter Name:' -TextColor 'Blue' -PropertyName 'UserName'
 .INPUTS
     [PSCustomObject]
-    [System.Windows.Forms.TabPage]
-    [System.String]
+    [System.Windows.Forms.GroupBox]
     [System.Int32]
-    [System.Windows.Forms.GroupBox]
-    [System.Management.Automation.SwitchParameter]
+    [System.String]
+    [System.Object[][]]
 .OUTPUTS
-    [System.Windows.Forms.GroupBox]
+    [System.Windows.Forms.TextBox]
 .NOTES
     This script is part of the Application Delivery Assistant. Copyright (C) Iotana. All rights reserved.
     Version         : 6.0.0.0
@@ -93,76 +89,183 @@ function Add-TextBoxDimensions {
     Last Update     : May 2026
 #>
 ####################################################################################################
-function New-GroupBox {
+function New-TextBox {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true,HelpMessage='The ApplicationObject containing the Settings.')]
-        [PSCustomObject]$InputObject,
+        [Parameter(Mandatory=$false,HelpMessage='The ApplicationObject containing the Settings.')]
+        [PSCustomObject]$ApplicationObject = $Global:ApplicationObject,
 
-        [Parameter(Mandatory=$true,HelpMessage='The Parent TabPage to which this groupbox will be added.')]
-        [System.Windows.Forms.TabPage]$ParentTabPage,
+        [Parameter(Mandatory=$true,HelpMessage='The Parent GroupBox to which this TextBox will be added.')]
+        [System.Windows.Forms.GroupBox]$ParentGroupBox,
 
-        [Parameter(Mandatory=$true,HelpMessage='The title of the Groupbox.')]
-        [System.String]$Title,
+        [Parameter(Mandatory=$false,HelpMessage='The RowNumber where the TextBox will be placed.')]
+        [System.Int32]$RowNumber = 1,
 
-        [Parameter(Mandatory=$false,HelpMessage='The height of the groupbox expressed in rownumbers.')]
-        [System.Int32]$NumberOfRows = 1,
+        [Parameter(Mandatory=$false,HelpMessage='The SizeType of the TextBox. This will influence only the width, not the height.')]
+        [ValidateSet('Small','Medium','Large')]
+        [System.String]$SizeType = 'Large',
 
-        [Parameter(Mandatory=$false,HelpMessage='The color of the text and border.')]
-        [System.String]$Color = 'Black',
+        [Parameter(Mandatory=$false,HelpMessage='The Type of TextBox. This will influence the TextBox background color, and the readonly property.')]
+        [ValidateSet('Input','Output')]
+        [System.String]$Type = 'Input',
 
-        [Parameter(Mandatory=$false,HelpMessage='The groupbox underneath which this new Groupbox will be placed.')]
-        [System.Windows.Forms.GroupBox]$GroupBoxAbove,
+        [Parameter(Mandatory=$false,HelpMessage='The Label that will be placed on the left of the TextBox.')]
+        [System.String]$Label,
 
-        [Parameter(Mandatory=$false,HelpMessage='Switch for when the GroupBox is on a SubTab.')]
-        [System.Management.Automation.SwitchParameter]$OnSubTab
+        [Parameter(Mandatory=$false,HelpMessage='The color of the text.')]
+        [System.String]$TextColor = 'Black',
+
+        [Parameter(Mandatory=$false,HelpMessage='The PropertyName that will be added to the object, to interact with the registry.')]
+        [System.String]$PropertyName,
+
+        [Parameter(Mandatory=$false,HelpMessage='The DefaultValue that will be added to the object.')]
+        [System.String]$DefaultValue,
+
+        [Parameter(Mandatory=$false,HelpMessage='The DefaultButtonsArray that will be added to the object.')]
+        [System.Object[][]]$ButtonPropertiesArray,
+
+        [Parameter(Mandatory=$false,HelpMessage='The ToolTip text to display when hovering over the TextBox.')]
+        [System.String]$ToolTip
     )
 
-    try {
-        # PREPARATION - GET SETTINGS
-        # Get the MainTabControl settings
-        [System.Collections.Hashtable]$Settings = $InputObject.GraphicalSettings
+    begin {
+        ####################################################################################################
+        ### MAIN PROPERTIES ###
 
+        # Input
+        [System.Collections.Hashtable]$Settings     = $ApplicationObject.Settings
 
-        # EXECUTION - CREATE THE GROUPBOX
-        # Create the GroupBox
-        [System.Windows.Forms.GroupBox]$NewGroupBox = New-Object System.Windows.Forms.GroupBox
+        # Create a new TextBox as the Output
+        [System.Windows.Forms.TextBox]$NewTextBox   = New-Object System.Windows.Forms.TextBox
+
+        ####################################################################################################
+    }
+    
+    process {
+        ####################################################################################################
+        ### NATIVE PROPERTIES ###
 
         # LOCATION
-        # Set the Location of the GroupBox
-        [System.Int32]$GroupboxTopLeftX = $Settings.MainTabControl.TopLeftX
-        [System.Int32]$GroupboxTopLeftY = if ($GroupBoxAbove) {
-            $GroupBoxAbove.Location.Y + $GroupBoxAbove.Height + $Settings.GroupBox.BetweenMargin
-        } else {
-            $Settings.MainTabControl.TopLeftY
-        }
-        [System.Int32[]]$Location = @($GroupboxTopLeftX, $GroupboxTopLeftY)
+        # Set the location
+        [System.Int32]$TextBoxTopLeftX  = $ParentGroupBox.Location.X + $Settings.TextBox.LeftMargin
+        [System.Int32]$TextBoxTopLeftY  = $Settings.TextBox.TopMargin + (($RowNumber - 1) * $Settings.TextBox.Height)
+        $NewTextBox.Location            = New-Object System.Drawing.Point($TextBoxTopLeftX, $TextBoxTopLeftY)
 
         # SIZE
-        # Set the Size of the GroupBox
-        [System.Int32]$Width    = if ($OnSubTab.IsPresent) {
-            $Settings.GroupBox.SubTabGroupboxWidth
-        } else {
-            $Settings.GroupBox.Width
+        # Set the size
+        [System.Int32]$TextBoxWidth = switch ($SizeType) {
+            'Large'     { $Settings.TextBox.LargeWidth }
+            'Medium'    { $Settings.TextBox.MediumWidth }
+            'Small'     { $Settings.TextBox.SmallWidth }
         }
-        [System.Int32]$Height   = $Settings.GroupBox.HeightTable.($NumberOfRows)
-        [System.Int32[]]$Size   = @($Width, $Height)
+        [System.Int32]$TextBoxHeight = $TextBoxTopLeftY + $Settings.TextBox.Height
+        $NewTextBox.Size = New-Object System.Drawing.Size($TextBoxWidth, $TextBoxHeight)
 
-        # ADD PROPERTIES
-        # Add the properties to the GroupBox
-        $NewGroupBox.Text       = $Title
-        $NewGroupBox.ForeColor  = $Color
-        $NewGroupBox.Location   = New-Object System.Drawing.Point($Location)
-        $NewGroupBox.Size       = New-Object System.Drawing.Size($Size)
+        # FONT
+        # Set the font
+        $NewTextBox.Font = $Settings.MainFont
+
+        # COLORS
+        # Set the BackColor
+        $NewTextBox.BackColor = switch ($Type) {
+            'Input'     { 'White' }
+            'Output'    { 'Beige' }
+        }
+        # Set the ForeColor
+        $NewTextBox.ForeColor = $TextColor
+
+        # READONLY
+        # Set the ReadOnly property
+        $NewTextBox.ReadOnly = switch ($Type) {
+            'Input'     { $false }
+            'Output'    { $true }
+        }
+
+        ####################################################################################################
+        ### CUSTOM PROPERTIES ###
+
+        # TAG
+        # Create the Tag property
+        $NewTextBox.Tag = [PSCustomObject]@{}
+
+        # LABEL
+        # Create the label and add it to the Tag property
+        if ($Label) {
+            New-Label -InputObject $ApplicationObject -ParentGroupBox $ParentGroupBox -Text $Label -RowNumber $RowNumber
+            $NewTextBox.Tag | Add-Member -MemberType NoteProperty -Name Label -Value $Label
+        }
+
+        # PROPERTYNAME
+        # Add the PropertyName
+        if ($PropertyName) {
+            $NewTextBox.Tag | Add-Member -MemberType NoteProperty -Name PropertyName -Value $PropertyName
+            # Make it interact with the registry
+            $NewTextBox.Text = Invoke-RegistrySettings -Read -PropertyName $NewTextBox.Tag.PropertyName
+            $NewTextBox.Add_TextChanged([System.EventHandler]{ param($TextBoxInternal=$NewTextBox) Invoke-RegistrySettings -Write -PropertyName $TextBoxInternal.Tag.PropertyName -PropertyValue $TextBoxInternal.Text })
+        }
+
+        # DEFAULTVALUE
+        # Add the DefaultValue
+        if ($DefaultValue) {
+            # Add the DefaultValue to the Tag property
+            $NewTextBox.Tag | Add-Member -MemberType NoteProperty -Name DefaultValue -Value $DefaultValue
+            # If the box is empty then fill it with the DefaultValue
+            if (Test-String -IsEmpty $NewTextBox.Text) {
+                Write-Line ("The box labeled ($($NewTextBox.Tag.Label)) is empty. It will be filled with the default value: ($DefaultValue)")
+                $NewTextBox.Text = $NewTextBox.Tag.DefaultValue
+            }
+        }
+
+        # BUTTONS
+        # Add the ButtonPropertiesArray
+        if ($ButtonPropertiesArray.Count -gt 0) {
+            try {
+                # Initialize the ButtonPropertiesArray in the Tag property
+                $NewTextBox.Tag | Add-Member -MemberType NoteProperty -Name ButtonPropertiesArray -Value @()
+                # Add each button to the ButtonPropertiesArray
+                foreach ($Button in $ButtonPropertiesArray) {
+                    # Set the button properties
+                    [System.Int32]$ColumnNumber = $Button[0]
+                    [System.String]$ButtonText  = $Button[1]
+                    # Create the hashtable
+                    [System.Collections.Hashtable]$ButtonHashtable = @{
+                        ColumnNumber    = $ColumnNumber
+                        Text            = $ButtonText
+                        Function        = switch ($ButtonText) {
+                            'Copy'      { { Invoke-ClipBoard -CopyFromBox $NewTextBox }.GetNewClosure() }
+                            'Paste'     { { Invoke-ClipBoard -PasteToBox $NewTextBox }.GetNewClosure() }
+                            'Clear'     { { Clear-TextBox $NewTextBox }.GetNewClosure() }
+                            'Default'   { { Reset-TextBoxToDefault $NewTextBox }.GetNewClosure() }
+                            'Browse'    { { [System.String]$FolderName = Select-Item -Folder ; if ($FolderName) { $NewTextBox.Text = $FolderName } }.GetNewClosure() }
+                            'Open'      { { Open-Folder -Path $NewTextBox.Text }.GetNewClosure() }
+                        }
+                    }
+                    # Add the hashtable to the ButtonPropertiesArray
+                    $NewTextBox.Tag.ButtonPropertiesArray += $ButtonHashtable
+                }
+                # Create the buttons
+                Invoke-ButtonLine -ButtonPropertiesArray $NewTextBox.Tag.ButtonPropertiesArray -ParentGroupBox $ParentGroupBox -RowNumber ($RowNumber + 1)
+            }
+            catch {
+                Write-FullError
+            }
+        }
+
+        # TOOLTIP
+        # Add the ToolTip
+        if ($ToolTip) {
+            [System.Windows.Forms.ToolTip]$TextBoxToolTip = New-Object System.Windows.Forms.ToolTip
+            $TextBoxToolTip.SetToolTip($NewTextBox, $ToolTip)
+        }
 
         # ADD TO PARENT
-        # Add the Groupbox to the ParentTabPage
-        $ParentTabPage.Controls.Add($NewGroupBox)
-        # Return the output
-        $NewGroupBox
+        # Add the new textbox to the parent
+        $ParentGroupBox.Controls.Add($NewTextBox)
     }
-    catch {
-        Write-ErrorReport -ErrorRecord $_
+
+    end {
+        # Return the output
+        $NewTextBox
     }
 }
 
