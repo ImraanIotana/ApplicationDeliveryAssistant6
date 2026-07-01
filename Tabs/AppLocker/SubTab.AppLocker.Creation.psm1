ee3306aa17a45598b095c04a147f7376ab3b3f80
Imraan Iotana <subpagina_ionisch.0a@icloud.com>
2026-07-01 07:52:10 +0200

    Import-FeatureAppLockerCreation

--- Tabs/AppLocker/SubTab.AppLocker.Creation.psm1
+++ Tabs/AppLocker/SubTab.AppLocker.Creation.psm1
@@ -1,6 +1,63 @@
 ####################################################################################################
 <#
 .SYNOPSIS
+    Imports the Creation sub-tab into the AppLocker tab.
+.DESCRIPTION
+    This function imports the Creation sub-tab into the AppLocker tab by creating a new TabPage and adding it to the specified parent TabControl.
+.EXAMPLE
+    Import-SubTabAppLockerCreation -InputObject $InputObject -ParentTabControl $MySubTabControl
+.INPUTS
+    [PSCustomObject]
+    [System.Windows.Forms.TabControl]
+.OUTPUTS
+    No objects are returned to the pipeline.
+.NOTES
+    This script is part of the Application Delivery Assistant. Copyright (C) Iotana. All rights reserved.
+    Version         : 6.0.0.0
+    Author          : Imraan Iotana
+    Creation Date   : June 2026
+    Last Update     : June 2026
+#>
+####################################################################################################
+function Import-SubTabAppLockerCreation {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true,HelpMessage='The ApplicationObject containing the Settings.')]
+        [PSCustomObject]$InputObject,
+
+        [Parameter(Mandatory=$true,HelpMessage='The Parent TabControl to which this TabPage will be added.')]
+        [System.Windows.Forms.TabControl]$ParentTabControl
+    )
+
+    try {
+        # PREPARATION
+        # Tab properties
+        [System.Collections.Hashtable]$TabProperties = @{
+            ParentTabControl    = $ParentTabControl
+            Title               = 'APPLOCKER CREATION'
+            Version             = '6.0.0.0'
+            BackGroundColor     = 'Blue'
+        }
+
+        # EXECUTION
+        # Create the TabPage
+        [System.Windows.Forms.TabPage]$ParentTabPage = New-TabPage @TabProperties
+
+        # Import the Features
+        $null = Import-FeatureAppLockerCreation -InputObject $InputObject -ParentTabPage $ParentTabPage -Color 'GreenYellow'
+    }
+    catch {
+        Write-ErrorReport -ErrorRecord $_
+    }
+}
+
+### END OF FUNCTION
+####################################################################################################
+
+
+####################################################################################################
+<#
+.SYNOPSIS
     Imports the AppLocker Creation feature into the AppLocker tab.
 .DESCRIPTION
     This function imports the AppLocker Creation feature into the AppLocker tab by creating a new GroupBox and adding it to the specified parent TabPage.
